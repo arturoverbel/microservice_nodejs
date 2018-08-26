c9200e96c9cdace3c86276a06eb332d22e3ab674
Arturo Verbel <arturo-verbel@hotmail.com>
2018-08-26 17:20:52 -0500

    Service created

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.json({ 
+        notas: {
+            "laboratorio_1": 5,
+            "laboratorio_2": 5,
+            "laboratorio_3": 5,
+            "blog": 5
+        }
+        
+    });
+});
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+    res.json({ 
+        notas: {
+            "laboratorio_1": 5,
+            "laboratorio_2": 5,
+            "laboratorio_3": 5,
+            "blog": 5
+        }
+        
+    });
 });
 
 module.exports = router;
